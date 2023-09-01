563f65ef4ba6256bdb99790b32ae740779c0b5e0
Heizel Kim <hrkim0624@gmail.com>
2023-09-01 15:06:04 +0900

    [SC-773] Apply lint.

--- tests/test_multi_process_db.py
+++ tests/test_multi_process_db.py
@@ -1,61 +1,61 @@
-from multiprocessing import Process
-
-import lmdb
 import os
-import pytest
-import rocksdb
+from multiprocessing import Process
 from pathlib import Path
-from rocksdb.errors import RocksIOError
 from shutil import rmtree
 from time import sleep
 
+import lmdb
+import pytest
+import rocksdb
+from rocksdb.errors import RocksIOError
+
 
 class TestDBPath:
-    rocksdb = 'test_rocksdb'
-    lmdb = 'test_lmdb'
+    rocksdb = "test_rocksdb"
+    lmdb = "test_lmdb"
 
 
 def print_process_info(title):
     print("\n--------------------------")
     print(title)
-    print('module name:', __name__)
-    print('parent process:', os.getppid())
-    print('process id:', os.getpid())
+    print("module name:", __name__)
+    print("parent process:", os.getppid())
+    print("process id:", os.getpid())
 
 
 def f_rocksdb(name: str):
-    print_process_info(f'function f_rocksdb({name})')
-    data = b'test_test_test_rocksdb'
+    print_process_info(f"function f_rocksdb({name})")
+    data = b"test_test_test_rocksdb"
 
     try:
         db = rocksdb.DB(TestDBPath.rocksdb, rocksdb.Options(create_if_missing=True))
-        db.put(b'name', data)
-        print('f_rocksdb as a Writer')
+        db.put(b"name", data)
+        print("f_rocksdb as a Writer")
     except RocksIOError:
         sleep(0.1)
         db = rocksdb.DB(TestDBPath.rocksdb, rocksdb.Options(create_if_missing=True), read_only=True)
         if data != db.get(b"name"):
             exit(-1)
-        print('f_rocksdb as a Reader')
+        print("f_rocksdb as a Reader")
     sleep(1)
 
 
 def f_lmdb(name: str):
-    print_process_info(f'function f_lmdb({name})')
-    data = b'test_test_test_lmdb'
+    print_process_info(f"function f_lmdb({name})")
+    data = b"test_test_test_lmdb"
 
-    if name == 'prop1':
+    if name == "prop1":
         env = lmdb.open(TestDBPath.lmdb)
         with env.begin(write=True) as txn:
-            txn.put(b'name', data)
-        print('f_lmdb as a Writer')
+            txn.put(b"name", data)
+        print("f_lmdb as a Writer")
     else:
         sleep(0.1)
         env = lmdb.open(TestDBPath.lmdb)
         with env.begin() as txn:
-            if data != txn.get(b'name'):
+            if data != txn.get(b"name"):
                 exit(-1)
-        print('f_lmdb as a Reader')
+        print("f_lmdb as a Reader")
     sleep(1)
 
 
@@ -64,7 +64,7 @@ def delete_test_db_dirs():
 
     for db_path in db_paths:
         if db_path.exists():
-            print(f'delete DB({db_path.resolve()})')
+            print(f"delete DB({db_path.resolve()})")
             rmtree(db_path)
 
 
@@ -80,10 +80,10 @@ class TestMultiProcessLevelDB:
 
     @pytest.mark.parametrize("store_process_function", store_process_functions)
     def test_multiprocessing_db(self, store_process_function):
-        p1 = Process(target=store_process_function, args=('prop1',))
+        p1 = Process(target=store_process_function, args=("prop1",))
         p1.start()
 
-        p2 = Process(target=store_process_function, args=('prop2',))
+        p2 = Process(target=store_process_function, args=("prop2",))
         p2.start()
 
         p1.join()
